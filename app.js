eb6df725221512814e499c9eb0d68a88a63b6458
Heather Forde <heatherforde1d@gmail.com>
2023-06-12 13:51:28 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+alert("Welcome Mrs. Wolfe");
+
+
+// WITH AN IF ELSE STATEMENT
+// if(accessNames.includes(yourName)) {
+//     window.location.href="/access-granted.html"
+// } else {
+//     window.location.href="/denied.html"
+// } 
+
+// $(".userName").innerText = "yourName";
+
+
+//& r.w access
+// var access = prompt("你好吗？")；
+
+// if(access == "happy"){ 
+//     window.location.href="/content/main.html"
+// } else {
+//     window.location.href="/denied.html"
+// }
