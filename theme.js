72eda584ff8629f27a8b56555e7cf7fe3d8bba42
Heather Forde <heatherforde1d@gmail.com>
2024-04-02 18:41:09 -0400

    add night theme button styles via js

--- theme.js
+++ theme.js
@@ -5,6 +5,7 @@ const themeButton = document.querySelectorAll(".theme-button");
 //TODO For later when changing link styles for night theme
 const linkButton = document.querySelectorAll("a");
 
+//~ DAY THEME FUNCTION
 function turnDay() {
   document.body.style.backgroundImage = "url(/assets/images/daytime-miami.png)";
 
@@ -39,5 +40,28 @@ function turnDay() {
   document.querySelectorAll(".story")[9].style.color = "rgb(87, 34, 48)";
   document.querySelectorAll(".story")[10].style.color = "rgb(87, 34, 48)";
 
+  // outfit section
   document.querySelectorAll(".outfit")[0].style.color = "rgb(147, 144, 145)";
+}
+
+//~ NIGHT THEME FUNCTION
+
+function turnNight() {
+  //TODO For later when changing link styles for night theme
+  // const linkButton = document.querySelectorAll("a");
+
+  // night theme background image
+  //   document.body.style.backgroundImage = "url(/assets/images/nighttime-miami.jpg)";
+
+  // button background colour
+  document.querySelectorAll("button")[0].style.backgroundColor = "teal";
+  document.querySelectorAll("button")[1].style.backgroundColor = "teal";
+
+  // button font colour
+  document.querySelectorAll("button")[0].style.color = "white";
+  document.querySelectorAll("button")[1].style.color = "white";
+
+  // button border colour
+  document.querySelectorAll("button")[0].style.borderColor = "white";
+  document.querySelectorAll("button")[1].style.borderColor = "white";
 }
