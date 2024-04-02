271de8bc4d6fba30bcc8e8352e92406cf6605666
Heather Forde <heatherforde1d@gmail.com>
2024-04-02 18:28:13 -0400

    add content day theme styling via js

--- theme.js
+++ theme.js
@@ -18,7 +18,26 @@ function turnDay() {
   document.querySelectorAll("button")[0].style.color = "rgb(87, 34, 48)";
   document.querySelectorAll("button")[1].style.color = "rgb(87, 34, 48)";
 
-  //button border colour
+  // button border colour
   document.querySelectorAll("button")[0].style.borderColor = "rgb(87, 34, 48)";
   document.querySelectorAll("button")[1].style.borderColor = "rgb(87, 34, 48)";
+
+  // content background color
+  document.getElementById("content-story").style.backgroundColor =
+    "#rgb(224, 214, 200)";
+
+  // story font colour
+  document.querySelectorAll(".story")[0].style.color = "rgb(87, 34, 48)";
+  document.querySelectorAll(".story")[1].style.color = "rgb(87, 34, 48)";
+  document.querySelectorAll(".story")[2].style.color = "rgb(87, 34, 48)";
+  document.querySelectorAll(".story")[3].style.color = "rgb(87, 34, 48)";
+  document.querySelectorAll(".story")[4].style.color = "rgb(87, 34, 48)";
+  document.querySelectorAll(".story")[5].style.color = "rgb(87, 34, 48)";
+  document.querySelectorAll(".story")[6].style.color = "rgb(87, 34, 48)";
+  document.querySelectorAll(".story")[7].style.color = "rgb(87, 34, 48)";
+  document.querySelectorAll(".story")[8].style.color = "rgb(87, 34, 48)";
+  document.querySelectorAll(".story")[9].style.color = "rgb(87, 34, 48)";
+  document.querySelectorAll(".story")[10].style.color = "rgb(87, 34, 48)";
+
+  document.querySelectorAll(".outfit")[0].style.color = "rgb(147, 144, 145)";
 }
