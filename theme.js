950905c82750387dd8ba4feef0d8065e191c3c79
Heather Forde <heatherforde1d@gmail.com>
2024-04-02 18:22:17 -0400

    add day theme button style via js

--- theme.js
+++ theme.js
@@ -1,1 +1,24 @@
-alert("What is up?");
+// alert("What is up?");
+
+const themeButton = document.querySelectorAll(".theme-button");
+
+//TODO For later when changing link styles for night theme
+const linkButton = document.querySelectorAll("a");
+
+function turnDay() {
+  document.body.style.backgroundImage = "url(/assets/images/daytime-miami.png)";
+
+  // button background colour
+  document.querySelectorAll("button")[0].style.backgroundColor =
+    "rgb(224, 214, 200)";
+  document.querySelectorAll("button")[1].style.backgroundColor =
+    "rgb(224, 214, 200)";
+
+  // button font colour
+  document.querySelectorAll("button")[0].style.color = "rgb(87, 34, 48)";
+  document.querySelectorAll("button")[1].style.color = "rgb(87, 34, 48)";
+
+  //button border colour
+  document.querySelectorAll("button")[0].style.borderColor = "rgb(87, 34, 48)";
+  document.querySelectorAll("button")[1].style.borderColor = "rgb(87, 34, 48)";
+}
